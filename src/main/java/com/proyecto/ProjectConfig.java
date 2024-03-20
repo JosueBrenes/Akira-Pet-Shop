21b37a016eba0f4124c103f75da3c15c8e40db6f
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2024-03-20 00:38:07 -0600

    Agregue un boton para administradores, una vez inicien sesion los dirige a la pagina admin

--- src/main/java/com/proyecto/ProjectConfig.java
+++ src/main/java/com/proyecto/ProjectConfig.java
@@ -1,12 +1,15 @@
 package com.proyecto;
 
 import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -54,7 +57,7 @@ public class ProjectConfig implements WebMvcConfigurer {
                 .authorizeHttpRequests((request) -> request
                 .requestMatchers("/", "/index", "/errores/**",
                         "/carrito/**", "/catalogo/**", "/reportes/**",
-                        "/registro/**", "/js/**", "/webjars/**", "/css/**","/contacto/**" )
+                        "/registro/**", "/js/**", "/webjars/**", "/css/**", "/contacto/**")
                 .permitAll()
                 .requestMatchers(
                         "/producto/nuevo", "/producto/guardar",
