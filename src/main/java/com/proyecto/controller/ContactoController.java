9233ff26892e74c5c281858e1cdf4e38b81aee25
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2024-03-19 22:41:26 -0600

    Se creo la pagina contacto y su estilo

---
+++ src/main/java/com/proyecto/controller/ContactoController.java
@@ -0,0 +1,32 @@
+package com.proyecto.controller;
+
+import com.proyecto.domain.Categoria;
+import com.proyecto.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.proyecto.service.CategoriaService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/contacto")
+public class ContactoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        var lista = productoService.getProductos(false);
+        model.addAttribute("productos", lista);
+        var categorias = categoriaService.getCategorias(true);
+        model.addAttribute("categorias", categorias);
+        return "/contacto/listado";
+    }
+}
